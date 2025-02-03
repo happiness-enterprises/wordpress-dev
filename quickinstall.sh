08b0afaba074a73b13bd55af7f845d1f5f411762
William Grim <490007+grimwm@users.noreply.github.com>
2025-02-03 02:11:24 -0600

    Add ability to install plugins.

--- quickinstall.sh
+++ quickinstall.sh
@@ -9,4 +9,15 @@ wp core install \
 	--admin_password=admin \
 	--admin_email=root@localhost.local
 
+plugins=($(echo $WORDPRESS_PLUGINS))
+for plugin in "${plugins[@]}" ; do
+	wp plugin install "${plugin}"
+	#wp plugin auto-updates enable "${plugin}"
+	#wp plugin activate "${plugin}"
+done
+
+wp plugin uninstall akismet
+wp plugin auto-updates enable --all
+wp plugin activate --all
+
 echo "Visit http://localhost:8080 in your browser and use admin/admin as the user/password."
